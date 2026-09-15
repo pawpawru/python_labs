fdb530e775ec7a1837b1e15f89faa929bae381c4
Александр Павлов <paw.al3xander@yandex.ru>
2026-09-16 00:13:27 +0300

    изменил README

--- src/lab_2/arrays.py
+++ src/lab_2/arrays.py
@@ -44,19 +44,21 @@ def flatten(mat: list[list | tuple]) -> list:
             for j in i:
                 row_major.append(j)
         return row_major
-# TEST min_max
-'''print(min_max([3, -1, 5, 5, 0]))
-print(min_max([42]))
-print(min_max([-5, -2, -9]))'''
-'print(min_max([]))'
-'''print(min_max([1.5, 2, 2.0, -3.1]))
-# TEST unique_sorted
-print(unique_sorted([3, 1, 2, 1, 3]))
-print(unique_sorted([]))
-print(unique_sorted([-1, -1, 0, 2, 2]))
-print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
-# TEST flatten
-print(flatten([[1, 2], [3, 4]]))
-print(flatten([[1, 2], (3, 4, 5)]))
-print(flatten([[1], [], [2, 3]]))'''
-print(flatten([[1, 2], "ab"]))
+    
+if __name__ == "__main__":
+    # TEST min_max
+    print(min_max([3, -1, 5, 5, 0]))
+    print(min_max([42]))
+    print(min_max([-5, -2, -9]))
+    print(min_max([]))
+    print(min_max([1.5, 2, 2.0, -3.1]))
+    # TEST unique_sorted
+    print(unique_sorted([3, 1, 2, 1, 3]))
+    print(unique_sorted([]))
+    print(unique_sorted([-1, -1, 0, 2, 2]))
+    print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+    # TEST flatten
+    print(flatten([[1, 2], [3, 4]]))
+    print(flatten([[1, 2], (3, 4, 5)]))
+    print(flatten([[1], [], [2, 3]]))
+    print(flatten([[1, 2], "ab"]))

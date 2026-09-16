919bc020e5f7e0b438037060865a045f3a487a23
Александр Павлов <paw.al3xander@yandex.ru>
2026-09-16 21:27:30 +0300

    сделал row_sums

--- src/lab_2/matrix.py
+++ src/lab_2/matrix.py
@@ -1,5 +1,5 @@
 def transpose(mat: list[list[float | int]]) -> list[list]:
-    '''
+    """
     Параметры:
         mat: матрица.
     
@@ -7,10 +7,76 @@ def transpose(mat: list[list[float | int]]) -> list[list]:
         Транспонированная матрица.
     
     Вызывает:
-        ValueError: если строки разной длины.
-    '''
+        ValueError: если строки разной длины (не прямоугольная матрица).
+    """
     if not mat:
         return []
-    if not all(len(i) == len(mat[0]) for i in mat):
+    elif not all(len(i) == len(mat[0]) for i in mat):
         raise ValueError("Есть строки разной длины")
-# функция ещё не доделана
+    else:
+        rows_len = len(mat)
+        columns_len = len(mat[0])
+        '''
+        чтобы проверить что это работает именно так я рассмотрел 2 матрицы:
+        1 2 3
+        4 5 6
+
+        1 2 3 4
+        5 6 7 8
+
+        можно заметить, что длина строки == количеству столбцов при условии, что все строки одной длины.
+        '''
+        transposed_mat = [[0] * rows_len for i in range(columns_len)]
+        for rows in range(rows_len):
+            for columns in range(columns_len):
+                transposed_mat[columns][rows] = mat[rows][columns]
+        return transposed_mat
+
+def row_sums(mat: list[list[float | int]]) -> list[float]:
+    """
+    Параметры:
+        mat: матрица.
+        
+    Возвращает:
+        Сумма каждой строки.
+        
+    Вызывает:
+        ValueError: если строки разной длины (не прямоугольная матрица).
+    """
+    if not mat:
+        return "Пустая матрица"
+    elif not all(len(i) == len(mat[0]) for i in mat):
+        raise ValueError("Есть строки разной длины")
+    else:
+        return [sum(i) for i in mat]
+
+def col_sums(mat: list[list[float | int]]) -> list[float]:
+    """
+    Параметры:
+        mat: матрица.
+        
+    Возвращает:
+        Сумма каждого столбца.
+        
+    Вызывает:
+        ValueError: если строки разной длины (не прямоугольная матрица).    
+    """
+    if not mat:
+            return "Пустая матрица"
+    elif not all(len(i) == len(mat[0]) for i in mat):
+        raise ValueError("Есть строки разной длины")
+    else:
+        # функция не доделана
+if __name__ == "__main__":
+    # TEST transpose
+    '''print(transpose([[1, 2, 3]]))
+    print(transpose([[1], [2], [3]]))
+    print(transpose([[1, 2], [3, 4]]))
+    print(transpose([]))
+    print(transpose([[1, 2], [3]]))
+    # TEST row_sums
+    print(row_sums([[1, 2, 3], [4, 5, 6]]))
+    print(row_sums([[-1, 1], [10, -10]]))
+    print(row_sums([[0, 0], [0, 0]]))
+    print(row_sums([[1, 2], [3]]))'''
+    # TEST col_sums

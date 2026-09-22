d6a7ce66a391658c88d84290852206524a7e3286
Александр Павлов <paw.al3xander@yandex.ru>
2026-09-22 23:31:57 +0300

    mmm

--- src/lab_2/matrix.py
+++ src/lab_2/matrix.py
@@ -59,7 +59,7 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
         return row_sums(transpose(mat))
                 
 if __name__ == "__main__":
-    # TEST transpose
+    '''# TEST transpose
     print('[[1, 2, 3]] ->', transpose([[1, 2, 3]]))
     print('[[1], [2], [3]] ->', transpose([[1], [2], [3]]))
     print('[[1, 2], [3, 4]] ->', transpose([[1, 2], [3, 4]]))
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     print('[[-1, 1], [10, -10]] ->', row_sums([[-1, 1], [10, -10]]))
     print('[[0, 0], [0, 0]] ->', row_sums([[0, 0], [0, 0]]))
     print('[[1, 2], [3]] ->', row_sums([[1, 2], [3]]))
-    # TEST col_sums
+    '''# TEST col_sums
     print('[[1, 2, 3], [4, 5, 6]] ->', col_sums([[1, 2, 3], [4, 5, 6]]))
     print('[[-1, 1], [10, -10]] ->', col_sums([[-1, 1], [10, -10]]))
     print('[[0, 0], [0, 0]] ->', col_sums([[0, 0], [0, 0]]))

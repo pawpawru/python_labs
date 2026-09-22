ee99c1cd144c46bde5e83ac82a7210c214d2a7eb
Александр Павлов <paw.al3xander@yandex.ru>
2026-09-22 19:24:58 +0300

    m

--- src/lab_2/README.md
+++ src/lab_2/README.md
@@ -1,39 +1,64 @@
 Лабораторная работа № 2
 
 Задача № 1 
-# TEST min_max
+'''
+def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+    """
+    Параметры:
+        nums: список чисел.
 
-![Скриншот задачи №1.1](../../images/lab_2/img_01.png)
+    Возвращает:
+        Кортеж из двух элементов: (минимум, максимум).
 
-# TEST min_max ошибка
+    Вызывает:
+        ValueError: если список пуст.
+    """
+    if not nums:
+        raise ValueError("Список пуст")
+    minimum = maximum = nums[0]
+    for x in nums[1:]:
+        if x < minimum:
+            minimum = x
+        if x > maximum:
+            maximum = x
+    return minimum, maximum
+'''
+'''
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    """
+    Параметры:
+        nums: список чисел.
 
-![Скриншот задачи №1.2](../../images/lab_2/img_02.png)
+    Возвращает:
+        Отсортированный список уникальных значений по возрастанию.
+    """
+    unique_nums = list(set(nums))
+    for i in range(1, len(unique_nums)):
+        key = unique_nums[i] # элемент, который хотим вставить
+        j = i - 1 # индекс последнего отсортированого элемента
+        while j >= 0 and unique_nums[j] > key: # если элемент > key, сдвигаем вправо
+            unique_nums[j + 1] = unique_nums[j]
+            j -= 1
+        unique_nums[j + 1] = key # вставляем нужный элемент в освободившееся место
+    return unique_nums
+'''
+'''
+def flatten(mat: list[list | tuple]) -> list:
+    """
+    Параметры:
+        mat: список списков/кортежей.
 
-# TEST unique_sorted
+    Возвращает:
+        Список, «расплющенный» по строкам (row-major).
 
-![Скриншот задачи №1.3](../../images/lab_2/img_03.png)
-
-# TEST flatten
-
-![Скриншот задачи №1.4](../../images/lab_2/img_04.png)
-
-Задача № 2
-# TEST transpose
-
-![Скриншот задачи №2.1](../../images/lab_2/img_05.png)
-
-# TEST row_sums
-
-![Скриншот задачи №2.2](../../images/lab_2/img_06.png)
-
-# TEST col_sums
-
-![Скриншот задачи №2.4](../../images/lab_2/img_08.png)
-
-Задача № 3
-# TEST format_record
-
-![Скриншот задачи №2.4](../../images/lab_2/img_09.png)
-Задача № 3
-
-![Скриншот задачи №2.4](../../images/lab_2/img_09.png)
+    Вызывает:
+        TypeError: если встретился элемент, не являющийся списком или кортежем.
+    """
+    row_major = []
+    for row in mat:
+        if not isinstance(row, (list, tuple)):
+            raise TypeError("Встретился элемент, который не является списком/кортежем")
+        for item in row:
+            row_major.append(item)
+    return row_major
+'''
--- src/lab_2/arrays.py
+++ src/lab_2/arrays.py
@@ -11,8 +11,13 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     """
     if not nums:
         raise ValueError("Список пуст")
-    return min(nums), max(nums)
-
+    minimum = maximum = nums[0]
+    for x in nums[1:]:
+        if x < minimum:
+            minimum = x
+        if x > maximum:
+            maximum = x
+    return minimum, maximum
 
 def unique_sorted(nums: list[float | int]) -> list[float | int]:
     """
@@ -22,7 +27,15 @@ def unique_sorted(nums: list[float | int]) -> list[float | int]:
     Возвращает:
         Отсортированный список уникальных значений по возрастанию.
     """
-    return sorted(set(nums))
+    unique_nums = list(set(nums))
+    for i in range(1, len(unique_nums)):
+        key = unique_nums[i] # элемент, который хотим вставить
+        j = i - 1 # индекс последнего отсортированого элемента
+        while j >= 0 and unique_nums[j] > key: # если элемент > key, сдвигаем вправо
+            unique_nums[j + 1] = unique_nums[j]
+            j -= 1
+        unique_nums[j + 1] = key # вставляем нужный элемент в освободившееся место
+    return unique_nums
 
 
 def flatten(mat: list[list | tuple]) -> list:
@@ -37,22 +50,21 @@ def flatten(mat: list[list | tuple]) -> list:
         TypeError: если встретился элемент, не являющийся списком или кортежем.
     """
     row_major = []
-    if not all(isinstance(i, (list, tuple)) for i in mat):
-        raise TypeError("Встретился элемент, который не является списком/кортежем")
-    else:
-        for i in mat:
-            for j in i:
-                row_major.append(j)
-        return row_major
+    for row in mat:
+        if not isinstance(row, (list, tuple)):
+            raise TypeError("Встретился элемент, который не является списком/кортежем")
+        for item in row:
+            row_major.append(item)
+    return row_major
     
 if __name__ == "__main__":
     # TEST min_max
     print('[3, -1, 5, 5, 0] ->', min_max([3, -1, 5, 5, 0]))
     print('[42] ->', min_max([42]))
     print('[-5, -2, -9] ->', min_max([-5, -2, -9]))
-    print('[] ->', min_max([]))
     print('[1.5, 2, 2.0, -3.1] ->', min_max([1.5, 2, 2.0, -3.1]))
-    # TEST unique_sorted
+    print('[] ->', min_max([]))
+    '''# TEST unique_sorted
     print('[3, 1, 2, 1, 3] ->', unique_sorted([3, 1, 2, 1, 3]))
     print('[] ->', unique_sorted([]))
     print('[-1, -1, 0, 2, 2] ->', unique_sorted([-1, -1, 0, 2, 2]))
@@ -61,4 +73,4 @@ if __name__ == "__main__":
     print('[[1, 2], [3, 4]] ->', flatten([[1, 2], [3, 4]]))
     print('[[1, 2], (3, 4, 5)] ->', flatten([[1, 2], (3, 4, 5)]))
     print('[[1], [], [2, 3]]->', flatten([[1], [], [2, 3]]))
-    print('[[1, 2], "ab"] ->', flatten([[1, 2], "ab"]))
+    print('[[1, 2], "ab"] ->', flatten([[1, 2], "ab"]))'''

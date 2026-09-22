add6f2777bb8b470a4e29ad7d9202a84c83b7976
Александр Павлов <paw.al3xander@yandex.ru>
2026-09-22 19:53:56 +0300

    m

--- src/lab_2/matrix.py
+++ src/lab_2/matrix.py
@@ -16,16 +16,6 @@ def transpose(mat: list[list[float | int]]) -> list[list]:
     else:
         rows_len = len(mat)
         columns_len = len(mat[0])
-        '''
-        чтобы проверить что это работает именно так я рассмотрел 2 матрицы:
-        1 2 3
-        4 5 6
-
-        1 2 3 4
-        5 6 7 8
-
-        можно заметить, что длина строки == количеству столбцов при условии, что все строки одной длины.
-        '''
         transposed_mat = [[0] * rows_len for i in range(columns_len)]
         for rows in range(rows_len):
             for columns in range(columns_len):
@@ -44,7 +34,7 @@ def row_sums(mat: list[list[float | int]]) -> list[float]:
         ValueError: если строки разной длины (не прямоугольная матрица).
     """
     if not mat:
-        return "Пустая матрица"
+        return []
     elif not all(len(i) == len(mat[0]) for i in mat):
         raise ValueError("Есть строки разной длины")
     else:
@@ -69,7 +59,7 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
         return row_sums(transpose(mat))
                 
 if __name__ == "__main__":
-    # TEST transpose
+    '''# TEST transpose
     print('[[1, 2, 3]] ->', transpose([[1, 2, 3]]))
     print('[[1], [2], [3]] ->', transpose([[1], [2], [3]]))
     print('[[1, 2], [3, 4]] ->', transpose([[1, 2], [3, 4]]))
@@ -80,7 +70,7 @@ if __name__ == "__main__":
     print('[[-1, 1], [10, -10]] ->', row_sums([[-1, 1], [10, -10]]))
     print('[[0, 0], [0, 0]] ->', row_sums([[0, 0], [0, 0]]))
     print('[[1, 2], [3]] ->', row_sums([[1, 2], [3]]))
-    # TEST col_sums
+    '''# TEST col_sums
     print('[[1, 2, 3], [4, 5, 6]] ->', col_sums([[1, 2, 3], [4, 5, 6]]))
     print('[[-1, 1], [10, -10]] ->', col_sums([[-1, 1], [10, -10]]))
     print('[[0, 0], [0, 0]] ->', col_sums([[0, 0], [0, 0]]))
